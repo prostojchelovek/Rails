db1f74e169ac6cdb7a71e179326dbc7580d671bf
prosojchelovek <alaev.musost@mail.ru>
2020-06-30 10:30:34 +0300

    task 7

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,28 +1,40 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: %i[index create]
-  before_action :find_question, only: %i[show destroy]
+  before_action :find_test, only: %i[new create]
+  before_action :find_question, only: %i[edit update show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def index
-    render plain: @test.questions.pluck(:description)
-  end
-
   def show
-    render plain: @question.description
   end
 
   def new
+    @question = @test.questions.new
   end
 
-  def destroy  #Многое перепробовал, но этот метод не работает, вместо delete вызывается get запрос
+  def edit
+  end
+
+  def destroy
     @question.destroy!
-    render plain: "#{@question.id} вопрос удален"
+    redirect_to @question.test
   end
 
   def create
-    @question = @test.questions.create!(question_params)
-    render plain: "Вопрос создан"
+    @question = @test.questions.new(question_params)
+
+    if @question.save
+      redirect_to @question.test
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question.test
+    else
+      render :edit
+    end
   end
 
   private
