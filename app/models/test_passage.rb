e761e8207be6dc327fa9a39ad4298d50db35aee7
prosojchelovek <alaev.musost@mail.ru>
2020-07-03 13:49:27 +0300

    task 8.1

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,8 +3,7 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_validation :before_validation_set_next_question, on: :update
+  before_validation :set_current_question
 
   def completed?
     current_question.nil?
@@ -26,14 +25,26 @@ class TestPassage < ApplicationRecord
     success_rate >= 85
   end
 
+   def number_current_question
+     test.questions.count - unpassed_questions.count
+   end
+
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def set_current_question
+    self.current_question = next_question
   end
 
-  def before_validation_set_next_question
-    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
+  def unpassed_questions
+    test.questions.order(:id).where('id > ?', current_question.id)
+  end
+
+  def next_question
+    if current_question
+      unpassed_questions.first
+    else
+      test.questions.first
+    end
   end
 
   def correct_answer?(answer_ids)
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,1 +1,1 @@
-TestGuru::VERSION = '0.8.0'.freeze
+TestGuru::VERSION = '0.8.1'.freeze
